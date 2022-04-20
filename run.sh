a414a1edc9aa05550d72ce4c43229aa75a5b327e
iwata naoki <a.tokai.wain@gmail.com>
2022-04-20 18:28:23 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
 java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $SCRIPT_PATH/checkstyle_rule.xml -o ./checkstyle_result.xml **/*.java
 cat ./checkstyle_result.xml
