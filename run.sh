028407cff25c10e9a75cbffeeba4b8bb04610a7f
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-20 18:41:48 +0900

    Update env

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo rule=$INPUT_CHECKSTYLE_RULE
+echo result=$INPUT_CHECKSTYLE_RESULT
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
-java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $SCRIPT_PATH/checkstyle_rule.xml -o ./checkstyle_result.xml **/*.java
+java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $INPUT_CHECKSTYLE_RULE -o $INPUT_CHECKSTYLE_RESULT **/*.java
 cat ./checkstyle_result.xml
 exit 0
