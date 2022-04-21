20c7c3d8bb56b1c15811c8f06ca461846c460c8a
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-21 15:24:23 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 cd $GITHUB_WORKSPACE
 
-echo workspace=$GITHUB_WORKSPACE
-echo workdir=$INPUT_WORKDIR
-echo rule=$INPUT_RULE
-echo result=$INPUT_RESULT
+echo GITHUB_WORKSPACE=$GITHUB_WORKSPACE
+echo INPUT_FILTER=$INPUT_WORKDIR
+echo INPUT_RULE=$INPUT_RULE
+echo INPUT_RESULT=$INPUT_RESULT
 echo "----"
 
-java -jar /checkstyle.jar -f xml -c $INPUT_RULE -o $INPUT_RESULT **/*.java
+find $INPUT_FILTER | xargs java -jar /checkstyle.jar -f xml -c $INPUT_RULE -o $INPUT_RESULT
 
 echo "----"
 cat $INPUT_RESULT
