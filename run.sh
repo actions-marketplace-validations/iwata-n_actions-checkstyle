fa7e4c34fb1121d60a464749fd6270923aabbeb0
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-21 10:37:30 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ echo rule=$INPUT_RULE
 echo result=$INPUT_RESULT
 echo "----"
 
+find $INPUT_WORKDIR
+
 java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $INPUT_RULE -o $INPUT_RESULT $INPUT_WORKDIR 
 
 echo "----"
