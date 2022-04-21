9b3e06159855de8182cf75d8864769a49edf0dc7
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-21 10:09:45 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+SCRIPT_PATH=$(cd $(dirname $0); pwd)
 
 cd $GITHUB_WORKSPACE
 
+echo workspace=$GITHUB_WORKSPACE
 echo workdir=$INPUT_WORKDIR
+echo script_path=$SCRIPT_PATH
 echo rule=$INPUT_RULE
 echo result=$INPUT_RESULT
-SCRIPT_PATH=$(cd $(dirname $0); pwd)
+
 java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $INPUT_RULE -o $INPUT_RESULT $INPUT_WORKDIR 
 cat $INPUT_RESULT
 exit 0
