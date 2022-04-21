99c72d39876274fdb227946bdf0a3a2e8490f756
Naoki Iwata <naoki.iwata@zozo.com>
2022-04-21 09:27:25 +0900

    Update inputs param

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo rule=$INPUT_CHECKSTYLE_RULE
-echo result=$INPUT_CHECKSTYLE_RESULT
+echo rule=$INPUT_RULE
+echo result=$INPUT_RESULT
 SCRIPT_PATH=$(cd $(dirname $0); pwd)
-java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $INPUT_CHECKSTYLE_RULE -o $INPUT_CHECKSTYLE_RESULT **/*.java
+java -jar $SCRIPT_PATH/checkstyle.jar -f xml -c $INPUT_RULE -o $INPUT_RESULT **/*.java
 cat ./checkstyle_result.xml
 exit 0
